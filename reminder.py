a641528ffd9b1218eaf290cf9105613a9f4d2bb7
Delon Khaerun Alief <16722052@mahasiswa.itb.ac.id>
2022-10-16 18:20:56 +0700

    initial commit

---
+++ reminder.py
@@ -0,0 +1,14 @@
+# TUGAS BESAR PENGENALAN KOMPUTASI
+# KU1102-31
+
+# Kelompok 13
+# 16722047 - Jessica Budiman
+# 16722052 - Delon Khaerun Alief
+# 16722117 - Yuba Felda Tustika
+# 16722167 - Selpia Anggraeni Permatasari
+# 16722292 - Maulida Rahma Britania
+
+# Deskripsi: Program ini akan mengingatkan user untuk melakukan sesuatu
+# KAMUS
+
+# ALGORITMA
