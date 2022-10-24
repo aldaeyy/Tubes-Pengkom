9219af1b1e2b388a585746c7a422ff4bc9efc1ca
Delon Khaerun Alief <16722052@mahasiswa.itb.ac.id>
2022-10-24 20:43:46 +0700

    bug dan ada yang terlewat gils

--- reminder 3.0.py
+++ reminder 3.0.py
@@ -49,6 +49,14 @@ def menu_utama():
     print("5. Cari Event")
     print("6. Keluar\n")
 
+def menu_lihat():
+    print("1. Lihat event hari ini")
+    print("2. Lihat event besok")
+    print("3. Lihat event minggu ini")
+    print("4. Lihat event terlewat")
+    print("5. Lihat semua event")
+    print("6. Kembali\n")
+
 def menu_ubah():
     print("1. Nama event")
     print("2. Tanggal event")
@@ -56,8 +64,7 @@ def menu_ubah():
     print("4. Prioritas")
     print("5. Lokasi")
     print("6. Tags")
-    print("7. Deskripsi")
-    print("8. Kembali ke menu utama\n")
+    print("7. Deskripsi\n")
 
 def menu_cari():
     print("1. Cari berdasarkan nama")
@@ -96,7 +103,7 @@ def lihat_event_hari_ini():
     ada = 0
     print("LIHAT EVENT HARI INI")
     for event in list_event:
-        if event.tanggal == datetime.date.today():
+        if event.sisa == 0:
             ada += 1
             print(event)
     
@@ -107,7 +114,8 @@ def lihat_event_besok():
     ada = 0
     print("LIHAT EVENT BESOK")
     for event in list_event:
-        if event.tanggal == datetime.date.today() + datetime.timedelta(days=1):
+        if event.sisa == 1:
+            ada += 1
             print(event)
     
     if ada == 0:
@@ -117,7 +125,8 @@ def lihat_event_minggu_ini():
     ada = 0
     print("LIHAT EVENT MINGGU INI")
     for event in list_event:
-        if event.tanggal >= datetime.date.today() and event.tanggal <= datetime.date.today() + datetime.timedelta(days=7):
+        if 0 <= event.sisa <= 7:
+            ada += 1
             print(event)
     
     if ada == 0:
@@ -127,7 +136,8 @@ def lihat_event_terlewat():
     ada = 0
     print("LIHAT EVENT TERLEWAT")
     for event in list_event:
-        if event.tanggal < datetime.date.today():
+        if event.sisa < 0:
+            ada += 1
             print(event)
     
     if ada == 0:
@@ -139,130 +149,160 @@ def lihat_semua_event():
         print(event)
 
 def lihat_event():
-    print("LIHAT EVENT")
-    print("1. Lihat event hari ini")
-    print("2. Lihat event besok")
-    print("3. Lihat event minggu ini")
-    print("4. Lihat event terlewat")
-    print("5. Lihat semua event")
-    print("6. Kembali\n")
-    pilihan = int(input("Masukkan pilihan: "))
-    if pilihan == 1:
-        lihat_event_hari_ini()
-    elif pilihan == 2:
-        lihat_event_besok()
-    elif pilihan == 3:
-        lihat_event_minggu_ini()
-    elif pilihan == 4:
-        lihat_event_terlewat()
-    elif pilihan == 5:
-        lihat_semua_event()
-    elif pilihan == 6:
-        return
-    else:
-        print("Pilihan tidak valid!\n")
+    while True:
+        print("LIHAT EVENT")
+        menu_lihat()
+        pilihan = int(input("Masukkan pilihan: "))
+        if pilihan == 1:
+            lihat_event_hari_ini()
+        elif pilihan == 2:
+            lihat_event_besok()
+        elif pilihan == 3:
+            lihat_event_minggu_ini()
+        elif pilihan == 4:
+            lihat_event_terlewat()
+        elif pilihan == 5:
+            lihat_semua_event()
+        elif pilihan == 6:
+            break
+        else:
+            print("Pilihan tidak valid!\n")
 
 def hapus_event():
     global jumlah_event
     print("HAPUS EVENT")
     lihat_semua_event()
-    nomor_event = int(input("Masukkan nomor event yang ingin dihapus: "))
-    print(list_event[nomor_event - 1])
-    konfirmasi = input("Apakah anda yakin ingin menghapus event ini? (Y/N): ").lower()
-    if konfirmasi == "y":
-        list_event.pop(nomor_event)
-        print("Event berhasil dihapus!\n")
-        jumlah_event -= 1
+    nomor_event = int(input("Masukkan nomor event yang ingin dihapus ('0' untuk batal hapus event): "))
+    if nomor_event == 0:
+        print("Batal hapus event\n")
     else:
-        print("Event tidak jadi dihapus!\n")
+        print(list_event[nomor_event - 1])
+        konfirmasi = input("Apakah anda yakin ingin menghapus event ini? (Y/N): ").lower()
+        if konfirmasi == "y":
+            list_event.pop(nomor_event - 1)
+            for i in range(nomor_event - 1, jumlah_event - 1):
+                list_event[i].nomor -= 1
+            print("Event berhasil dihapus!\n")
+            jumlah_event -= 1
+        else:
+            print("Event tidak jadi dihapus!\n")
 
 def ubah_event():
     print("UBAH EVENT")
-    lihat_semua_event()
-    nomor_event = int(input("Masukkan nomor event yang ingin diubah: "))
-    print(list_event[nomor_event - 1])
-    menu_ubah()
-    pilihan = int(input("Masukkan pilihan yang ingin diubah: "))
-    if pilihan == 1:
-        nama = input("Masukkan nama event: ").upper()
-        list_event[nomor_event - 1].nama = nama
-    elif pilihan == 2:
-        tanggal_event = int(input("Masukkan tanggal event: "))
-        bulan_event = int(input("Masukkan bulan event: "))
-        tahun_event = int(input("Masukkan tahun event: "))
-        sisa_hari = (datetime.date(tahun_event, bulan_event, tanggal_event) - datetime.date.today()).days
-        tanggal = datetime.date(tahun_event, bulan_event, tanggal_event).strftime("%A, %d %B %Y")
-        list_event[nomor_event - 1].sisa = sisa_hari
-        list_event[nomor_event - 1].tanggal = tanggal
-    elif pilihan == 3:
-        pilihan2 = input("Apakah anda ingin menginputkan waktu event? (Y/N): ").lower()
-        if pilihan2 == "y":
-            jam_mulai = int(input("Masukkan jam mulai event: "))
-            menit_mulai = int(input("Masukkan menit mulai event: "))
-            waktu_mulai = datetime.time(jam_mulai, menit_mulai)
-            jam_selesai = int(input("Masukkan jam selesai event: "))
-            menit_selesai = int(input("Masukkan menit selesai event: "))
-            waktu_selesai = datetime.time(jam_selesai, menit_selesai)
-            waktu = f"{waktu_mulai} - {waktu_selesai}"
+    while True:
+        lihat_semua_event()
+        nomor_event = int(input("Masukkan nomor event yang ingin diubah ('0' untuk kembali ke menu utama): "))
+        if nomor_event == 0:
+            break
         else:
-            waktu = "Seharian"
-        list_event[nomor_event - 1].waktu = waktu
-    elif pilihan == 4:
-        pilihan2 = input("Ketik '1' untuk prioritas rendah, '2' untuk prioritas sedang, dan '3' untuk prioritas tinggi: ")
-        if pilihan2 == "1":
-            prioritas = "Rendah"
-        elif pilihan2 == "2":
-            prioritas = "Sedang"
-        elif pilihan2 == "3":
-            prioritas = "Tinggi"
-        else:
-            prioritas = None
-        list_event[nomor_event - 1].prioritas = prioritas
-    elif pilihan == 5:
-        list_event[nomor_event - 1].lokasi = input("Masukkan lokasi event: ")
-    elif pilihan == 6:
-        list_event[nomor_event - 1].tags = input("Masukkan tags event: ")
-    elif pilihan == 7:
-        list_event[nomor_event - 1].deskripsi = input("Masukkan deskripsi event: ")
-    elif pilihan == 8:
-        return
-    else:
-        print("Pilihan tidak valid!\n")
-    print("Event berhasil diubah!\n")
+            print(list_event[nomor_event - 1])
+            menu_ubah()
+            pilihan = int(input("Masukkan pilihan yang ingin diubah: "))
+            if pilihan == 1:
+                nama = input("Masukkan nama event: ").upper()
+                list_event[nomor_event - 1].nama = nama
+            elif pilihan == 2:
+                tanggal_event = int(input("Masukkan tanggal event: "))
+                bulan_event = int(input("Masukkan bulan event: "))
+                tahun_event = int(input("Masukkan tahun event: "))
+                sisa_hari = (datetime.date(tahun_event, bulan_event, tanggal_event) - datetime.date.today()).days
+                tanggal = datetime.date(tahun_event, bulan_event, tanggal_event).strftime("%A, %d %B %Y")
+                list_event[nomor_event - 1].sisa = sisa_hari
+                list_event[nomor_event - 1].tanggal = tanggal
+            elif pilihan == 3:
+                pilihan2 = input("Apakah anda ingin menginputkan waktu event? (Y/N): ").lower()
+                if pilihan2 == "y":
+                    jam_mulai = int(input("Masukkan jam mulai event: "))
+                    menit_mulai = int(input("Masukkan menit mulai event: "))
+                    waktu_mulai = datetime.time(jam_mulai, menit_mulai)
+                    jam_selesai = int(input("Masukkan jam selesai event: "))
+                    menit_selesai = int(input("Masukkan menit selesai event: "))
+                    waktu_selesai = datetime.time(jam_selesai, menit_selesai)
+                    waktu = f"{waktu_mulai} - {waktu_selesai}"
+                else:
+                    waktu = "Seharian"
+                list_event[nomor_event - 1].waktu = waktu
+            elif pilihan == 4:
+                pilihan2 = input("Ketik '1' untuk prioritas rendah, '2' untuk prioritas sedang, dan '3' untuk prioritas tinggi: ")
+                if pilihan2 == "1":
+                    prioritas = "Rendah"
+                elif pilihan2 == "2":
+                    prioritas = "Sedang"
+                elif pilihan2 == "3":
+                    prioritas = "Tinggi"
+                else:
+                    prioritas = None
+                list_event[nomor_event - 1].prioritas = prioritas
+            elif pilihan == 5:
+                list_event[nomor_event - 1].lokasi = input("Masukkan lokasi event: ")
+            elif pilihan == 6:
+                list_event[nomor_event - 1].tags = input("Masukkan tags event: ")
+            elif pilihan == 7:
+                list_event[nomor_event - 1].deskripsi = input("Masukkan deskripsi event: ")
+            elif pilihan == 8:
+                break
+            else:
+                print("Pilihan tidak valid!\n")
+            print("Event berhasil diubah!\n")
 
 def cari_event():
     print("CARI EVENT")
-    menu_cari()
-    pilihan = input("Masukkan pilihan: ")
-    if pilihan == "1":
-        nama = input("Masukkan nama event: ").upper()
-        for event in list_event:
-            if event.nama == nama:
-                print(event)
-    elif pilihan == "2":
-        tanggal_event = int(input("Masukkan tanggal event: "))
-        bulan_event = int(input("Masukkan bulan event: "))
-        tahun_event = int(input("Masukkan tahun event: "))
-        tanggal = datetime.date(tahun_event, bulan_event, tanggal_event).strftime("%A, %d %B %Y")
-        for event in list_event:
-            if event.tanggal == tanggal:
-                print(event)
-    elif pilihan == "3":
-        prioritas = input("Ketik '1' untuk prioritas rendah, '2' untuk prioritas sedang, dan '3' untuk prioritas tinggi: ")
-        if prioritas == "1":
+    while True:            
+        menu_cari()
+        pilihan = input("Masukkan pilihan: ")
+        ada = 0
+        if pilihan == "1":
+            nama = input("Masukkan nama event: ").upper()
             for event in list_event:
-                if event.prioritas == "Rendah":
+                if event.nama == nama:
+                    ada += 1
                     print(event)
-        elif prioritas == "2":
+            if ada == 0:
+                print("Tidak ada event dengan nama tersebut!\n")
+        elif pilihan == "2":
+            tanggal_event = int(input("Masukkan tanggal event: "))
+            bulan_event = int(input("Masukkan bulan event: "))
+            tahun_event = int(input("Masukkan tahun event: "))
+            tanggal = datetime.date(tahun_event, bulan_event, tanggal_event).strftime("%A, %d %B %Y")
             for event in list_event:
-                if event.prioritas == "Sedang":
+                if event.tanggal == tanggal:
+                    ada += 1
                     print(event)
-        elif prioritas == "3":
+            if ada == 0:
+                print("Tidak ada event dengan tanggal tersebut!\n")
+        elif pilihan == "3":
+            prioritas = input("Ketik '1' untuk prioritas rendah, '2' untuk prioritas sedang, dan '3' untuk prioritas tinggi: ")
+            if prioritas == "1":
+                for event in list_event:
+                    if event.prioritas == "Rendah":
+                        ada += 1
+                        print(event)
+            elif prioritas == "2":
+                for event in list_event:
+                    if event.prioritas == "Sedang":
+                        ada += 1
+                        print(event)
+            elif prioritas == "3":
+                for event in list_event:
+                    if event.prioritas == "Tinggi":
+                        ada += 1
+                        print(event)
+            else:
+                print("Prioritas tidak valid!\n")
+            if ada == 0:
+                print("Tidak ada event dengan prioritas tersebut!\n")
+        elif pilihan == "4":
+            tags = input("Masukkan tags event: ")
             for event in list_event:
-                if event.prioritas == "Tinggi":
+                if event.tags == tags:
+                    ada += 1
                     print(event)
+            if ada == 0:
+                print("Tidak ada event dengan tags tersebut!\n")
+        elif pilihan == "5":
+            break
         else:
-            print("Prioritas tidak valid!\n")
+            print("Pilihan tidak valid!\n")
 
 # ALGORITMA
 import datetime
